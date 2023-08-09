61014a640d9bbe3d623372dad7f6359ccf331d0e
odlomax <oliver.lomax@metoffice.gov.uk>
2023-08-09 13:52:40 +0100

    TDD example as of the end of the demo.

--- stack.h
+++ stack.h
@@ -1,2 +1,24 @@
 #pragma once
 
+#include <stdexcept>
+
+template <typename T, int MaxSize = 100>
+class Stack{
+ public:
+  auto size() const {return size_;}
+
+  auto push (double val) {
+    values[size_++] = val;
+  }
+
+  auto pop() {
+    if (!size_) throw std::out_of_range("empty stack");
+    return values[--size_];
+  }
+
+ private:
+
+  int size_{0};
+  T values[MaxSize];
+
+};
--- test.cpp
+++ test.cpp
@@ -1,10 +1,31 @@
-
 #define BOOST_TEST_MAIN
 
 #ifdef BOOST_TEST_DYN_LINK
-#include "boost/test/unit_test.hpp"
+#include <boost/test/unit_test.hpp>
 #else
-#include "boost/test/included/unit_test.hpp"
+#include <boost/test/included/unit_test.hpp>
 #endif
 
 #include "stack.h"
+
+BOOST_AUTO_TEST_CASE(emptyStack) {
+
+  auto stack = Stack<double>{};
+  BOOST_CHECK(stack.size() == 0);
+  BOOST_CHECK_THROW(stack.pop(), std::out_of_range);
+
+}
+
+BOOST_AUTO_TEST_CASE(testPushPop) {
+
+  auto stack = Stack<double>{};
+  stack.push(2.);
+  stack.push(3.);
+
+  BOOST_CHECK(stack.size() == 2);
+
+
+  BOOST_CHECK(stack.pop() == 3.);
+  BOOST_CHECK(stack.size() == 1);
+
+}
